d4609d475be4e2805c0921c902766dfc56106a17
Facundo Perez <facundoaperez@outlook.com.ar>
2023-04-08 02:24:04 -0300

    feat: agrego botones de play y reset

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 //let board = document.getElementById("board");
 const rows = 20;
 const columns = 40;
+let isPlaying = false;
+let grid;
 
 function cellOnClickHandler () {
     if(this.className === 'alive'){
@@ -10,28 +12,69 @@ function cellOnClickHandler () {
     this.setAttribute("class","alive");
 }
 
+function startOnClickHandler() {
+    
+    if(isPlaying){
+        isPlaying = false;
+        console.log("Stoping game!");
+        return;
+    }
+    isPlaying = true;
+    console.log("Playing game!");
+}
+
+function setupButtons(grid) {
+    let startButton = document.getElementById("start-button");
+    let resetButton = document.getElementById("reset-button");
+    startButton.onclick = startOnClickHandler;
+    resetButton.onclick = resetBoard;
+}
+
 const createBoard = () => {
     let boardContainer = document.getElementById("board");
-
+    let arr = new Array(columns);
     if(!boardContainer){
        throw Error("No board container");
     }
 
     let board = document.createElement("table");
+    
     for(let i = 0; i < rows; i++){
         let tr = document.createElement("tr");
+        arr[i] = new Array(rows);
         for(let j = 0; j < columns; j++){
             let cell = document.createElement("td");
             cell.setAttribute("id", i + "-" + j); 
             cell.setAttribute("class", "dead");
             cell.onclick = cellOnClickHandler;
             tr.appendChild(cell);
+            arr[i][j] = 0;
         }
         board.appendChild(tr);
     }
     boardContainer.appendChild(board);
-
+    return arr;
 }
 
+const resetBoard = () => {
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < columns; j++){
+            cell = document.getElementById(i+"-"+j);
+            cell.setAttribute("class", "dead");
+            grid[i][j] = 0;
+        }
+    }
+    console.log("Reset game!");
+}
 
-createBoard();
+const initialize = () => {
+
+    grid = createBoard();
+    setupButtons();
+
+    // while(isPlaying){
+    //     //TO DO
+    // }
+}
+
+initialize();
